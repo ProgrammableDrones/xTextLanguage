f062539185c04fe75e53cc3510f7a90d82b2159e
axelronquist <aronquist@gmail.com>
2017-05-03 14:54:57 +0100

    updated xdrone-deploy.sh

--- xdrone-deploy.sh
+++ xdrone-deploy.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-echo "var arDrone = require('ar-drone');
-var client  = arDrone.createClient();
-
-client.takeoff();
-
-client
-  .after(500, function() {
-    this.stop();
-    this.land();
-  });" > /tmp/result.js
-
 /usr/local/bin/node /tmp/result.js
 
 /usr/bin/killall node
